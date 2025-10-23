75ee29a2c470a45bf7fb795162036013ddbe7c0c
MUTHUSURESH N <muthusuresh.240198@aids.ritchennai.edu.in>
2025-10-23 18:58:48 +0530

    updated

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -39,7 +39,7 @@ const Contact = () => {
     }
 
     try {
-      const response = await fetch('http://localhost:3001/api/contact', {
+      const response = await fetch('/api/contact', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
